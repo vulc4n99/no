3d9d53bae98ea32561dd58ada7973ced6ac8edbf
Sahil Akalwadi <98448159+vulc4n99@users.noreply.github.com>
2025-07-09 11:28:52 +0000

    commiting

---
+++ app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+if __name__ == "__main__":
+  print(f"5 + 3 = {add(5, 3)}")
+  print(f"5 - 3 = {subtract(5, 3)}")
---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(2, 5) == -3
+    assert subtract(0, 0) == 0
